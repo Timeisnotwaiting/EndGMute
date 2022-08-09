3428c2fe4108a2d1fe3823d1f5cc728d506453f3
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-08-10 00:46:11 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -7,6 +7,7 @@ async def get_id(_: Client, m: Message):
             id = m.reply_to_message.from_user.id
         except:
             id = None
+            return await m.reply("User is with channel or with anonymous rights !")
     elif (not m.reply_to_message and len(m.command) > 1):
         text = m.text.split(None, 1)[1]
         if text[0] == "@":
@@ -15,4 +16,6 @@ async def get_id(_: Client, m: Message):
             except:
                 id = None
                 return await m.reply("Give username or id !")
+    else:
+        await m.reply(/mute | /unmute @----- | id ")
         
