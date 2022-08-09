6a78af183125171948ee655fede7a13135faf7a3
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-08-09 22:39:30 +0530

    Create mute_db.py

---
+++ database/mute_db.py
@@ -0,0 +1,15 @@
+from . import db
+
+mutedb = db.mute
+
+async def mute_user(a: int):
+    muted = mutedb.find_one({"a": a})
+    if not muted:
+        return await mutedb.insert_one({"a": a})
+    return 
+
+async def unmute_user(a: int):
+    muted = mutedb.find_one({"a": a})
+    if muted:
+        return await mutedb.delete_one({"a": a})
+    return
