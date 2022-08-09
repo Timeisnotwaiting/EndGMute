f2609c6d607e6c7b3a9432d99badace8761c8702
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-08-10 00:43:44 +0530

    Create helper.py

---
+++ helper.py
@@ -0,0 +1,18 @@
+from pyrogram import Client
+from pyrogram.types import Message
+
+async def get_id(_: Client, m: Message):
+    if m.reply_to_message:
+        try:
+            id = m.reply_to_message.from_user.id
+        except:
+            id = None
+    elif (not m.reply_to_message and len(m.command) > 1):
+        text = m.text.split(None, 1)[1]
+        if text[0] == "@":
+            try:
+                id = (await _.get_users(text)).id
+            except:
+                id = None
+                return await m.reply("Give username or id !")
+        
