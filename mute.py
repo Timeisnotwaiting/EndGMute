8bfff346e63dc54fafeac5077442a83d89b8d52e
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-08-10 00:57:33 +0530

    Update mute.py

--- mute.py
+++ mute.py
@@ -1,6 +1,7 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from config import *
+from helper import get_id
 
 alpha = Client(":Alpha:", API_ID, API_HASH, BOT_TOKEN)
 
@@ -9,4 +10,18 @@ async def gmute(_, m):
     sudo = await is_sudo(m.from_user.id)
     if not sudo:
         return
+    id, r = await get_id(_, m)
+    if not id:
+        return await m.reply(r)
+    muted = await is_muted(id)
+    if not muted:
+        ok = await m.reply("muting user.... ")
+        try:
+            await mute_user(id)
+            await ok.edit(f"{(await _.get_users(id)).mention} gmuted !")
+        except Exception as e:
+            await ok.edit(f"can't add user id to database...\n\nError :- {e}")
+    else:
+        await m.reply("This user is already gmuted.... !")
+    
     
