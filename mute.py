8c3df90db24971b38d6b331f13b0e6caa8f0cbad
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-08-10 00:24:48 +0530

    Update mute.py

--- mute.py
+++ mute.py
@@ -4,4 +4,8 @@ from config import *
 
 alpha = Client(":Alpha:", API_ID, API_HASH, BOT_TOKEN)
 
-
+@alpha.on_message(filters.command("gmute") & ~filters.edited)
+async def gmute(_, m):
+    sudo = await is_sudo(m.from_user.id)
+    if not sudo:
+        return
