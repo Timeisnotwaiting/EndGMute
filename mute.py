8e844e8e8299151fcb23ff07090cdbd28d28624c
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-09-15 14:24:14 +0530

    Update mute.py

--- mute.py
+++ mute.py
@@ -1,39 +1,11 @@
 from pyrogram import Client, filters, idle
 from pyrogram.types import Message
 from config import *
+from helpers import get_id
 from database.client import *
 
 
 alpha = Client(":Alpha:", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
-
-async def get_id(_: Client, m: Message):
-    if m.reply_to_message:
-        try:
-            id = m.reply_to_message.from_user.id
-            r = None 
-        except:
-            id = None
-            r = "User is with channel or with anonymous rights !"
-    elif (not m.reply_to_message and len(m.command) > 1):
-        text = m.text.split(None, 1)[1]
-        if text[0] == "@":
-            try:
-                id = (await _.get_users(text)).id
-                r = None
-            except:
-                id = None
-                r = "Give username or id !"
-        elif text.isnumeric():
-            id = int(text)
-            r = None
-        else:
-            id = None
-            r = "Give username or id !"
-    else:
-        id = None 
-        r = "/mute | /unmute @----- | id "
-
-    return id, r
 
 @alpha.on_message(filters.command(["gmute", "ungmute"]))
 async def gmute(_, m):
