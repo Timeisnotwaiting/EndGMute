bddf8c2c74964e185bdee65755f0dd7bc5f467e1
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-09-16 15:34:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,3 +4,4 @@ API_ID = os.environ['API_ID']
 API_HASH = os.environ['API_HASH']
 BOT_TOKEN = os.environ['BOT_TOKEN']
 MONGO_DB_URL = os.environ['MONGO_DB_URL']
+OWNER_ID = int(os.environ['OWNER_ID'])
