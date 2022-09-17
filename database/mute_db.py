3d374f2d0de00a86f14ba8e25a7b747589699dbe
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-09-17 09:58:54 +0530

    Update mute_db.py

--- database/mute_db.py
+++ database/mute_db.py
@@ -13,6 +13,36 @@ class Mute(BASE):
 
 Mute.__table__.create(checkfirst=True)
 
+def mute_user(id):
+    muted = SESSION.query(Mute).get(id)
+    if muted:
+        return SESSION.close()
+    with threading.RLock():
+        SESSION.add(Mute(id))
+        return SESSION.commit()
 
-    
+def unmute_user(id):
+    muted = SESSION.query(Mute).get(id)
+    if not muted:
+        return SESSION.close()
+    with threading.RLock():
+        SESSION.delete(muted)
+        return SESSION.commit()
+
+def is_muted(id):
+    muted = SESSION.query(Mute).get(id)
+    if muted:
+        return True
+    return False
+
+def get_muted():
+    all = SESSION.query(Mute).all()
+    if not all:
+        return []
+    Hehe = []
+    for x in all:
+        Hehe.append(x["id"])
+    return Hehe
+
+
     
