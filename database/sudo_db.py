2bc1d2e7dbe2694735b4adec52cde5bb6b6c7575
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-09-17 11:07:11 +0530

    Update sudo_db.py

--- database/sudo_db.py
+++ database/sudo_db.py
@@ -11,8 +11,36 @@ class Sudo(BASE):
 
 Sudo.__table__.create(checkfirst=True)
 
+def add_sudo(id):
+    sudo = SESSION.query(Sudo).get(id)
+    if sudo:
+        return SESSION.close()
+    with threading.RLock():
+        SESSION.add(Sudo(id))
+        return SESSION.commit()
 
+def del_sudo(id):
+    sudo = SESSION.query(Sudo).get(id)
+    if not sudo:
+        return SESSION.close()
+    with threading.RLock():
+        SESSION.delete(sudo)
+        return SESSION.commit()
 
+def is_sudo(id):
+    sudo = SESSION.query(Sudo).get(id)
+    if sudo:
+        return True
+    return False
+
+def get_sudos():
+    sudos = SESSION.query(Sudo).all()
+    if not sudos:
+        return []
+    Lmao = []
+    for sudo in sudos:
+        Lmao.append(sudo["id"])
+    return Lmao
 
 
     
