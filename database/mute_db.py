61bb565ec1ce8455faca7845722c5e2cfcc1c6fa
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-09-17 09:45:52 +0530

    Update mute_db.py

--- database/mute_db.py
+++ database/mute_db.py
@@ -1,2 +1,18 @@
 from . import BASE, SESSION
+from sqlalchemy import Column
+from sqlalchemy.sql.sqltypes import BigInteger
+import threading
 
+class Mute(BASE):
+    __tablename__ = "mute"
+
+    id = Column(BigInteger, primary_key=True)
+
+    def __init__(self, id):
+        self.id = id
+
+Mute.__table__.create(checkfirst=True)
+
+
+    
+    
