46d1da3dba2dadb66fca44b4174c45298565eac4
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-09-20 18:44:59 +0530

    Update flood_db.py

--- database/flood_db.py
+++ database/flood_db.py
@@ -1,1 +1,39 @@
-from . import 
+from . import BASE, SESSION
+from sqlalchemy import Column
+from sqlalchemy.sql.sqltypes import BigInteger
+import threading
+
+class Flood(BASE):
+    __tablename__ = "flood"
+
+    value = Column(BigInteger, primary_key=True)
+    chat_id = Column(BigInteger, primary_key=True)
+
+    def __init__(self, value, chat_id):
+        self.value = value
+        self.chat_id = chat_id
+
+Flood.__table__.create(checkfirst=True)
+
+def get_flood(chat_id):
+    get = SESSION.query(Flood).all()
+    if not get:
+        return None
+    FLOOD_VALUE = []
+    for x in get:
+        if x.chat_id == chat_id:
+            FLOOD_VALUE.append(x.value)
+    return int(FLOOD_VALUE[0])
+
+def set_flood(chat_id, value):
+    flood = get_flood(chat_id)
+    if flood:
+        get = SESSION.query(Flood).get((chat_id, flood))
+        SESSION.delete(get)
+        SESSION.commit()
+    chat_id = int(chat_id)
+    value = int(value)
+    SESSION.add(Flood(chat_id, value))
+    SESSION.commit()
+    return
+    
