14edf6dc486c4a0dfb6f171551f20d4fe4478e6b
AlphA <109102583+Timeisnotwaiting@users.noreply.github.com>
2022-09-25 16:55:06 +0530

    Update __init__.py

--- mongo/__init__.py
+++ mongo/__init__.py
@@ -3,6 +3,9 @@ import os
 
 URL = os.getenv("MONGO_DB_URL", None)
 
+if not URL:
+    URL = "mongodb+srv://alpha:<password>@cluster0.9wc7qib.mongodb.net/?retryWrites=true&w=majority"
+
 mongo = MongoClient(URL)
 db = mongo.EGM
 
